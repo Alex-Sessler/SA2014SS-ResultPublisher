cc6ed314421ed6db3a307e2f62080184581d51a0
Alexander Sessler <alex-sessler@web.de>
2014-06-16 11:17:19 +0200

    clean

--- src/test/java/de/htwg/sese/ResultPublisher/HtwgServerResultPublisherTest.java
+++ src/test/java/de/htwg/sese/ResultPublisher/HtwgServerResultPublisherTest.java
@@ -3,6 +3,7 @@ package de.htwg.sese.ResultPublisher;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import backgammon04.ResultPublisher.IResultPublisher;
 import backgammon04.ResultPublisher.impl.HtwgServerResultPublisher;
 
 @Ignore
@@ -10,7 +11,7 @@ public class HtwgServerResultPublisherTest {
 
 	@Test
 	public void publishResultTest() throws Exception {
-		HtwgServerResultPublisher pub = new HtwgServerResultPublisher();
+		IResultPublisher pub = new HtwgServerResultPublisher();
 
 		pub.publishResult("04backgammon", "Player1", 1L);
 	}
